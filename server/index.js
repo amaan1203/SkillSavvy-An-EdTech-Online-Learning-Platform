8749b03912c5bca51f08f3bd1d4ebf5bd6d6fed6
amaan1203 <127188069+amaan1203@users.noreply.github.com>
2024-07-08 16:31:03 +0530

    cors origin changed

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: 'http://localhost:3000',
+        origin: 'https://skill-savvy-an-ed-tech-frontend.vercel.app',
         credentials: true,
     })
 )
